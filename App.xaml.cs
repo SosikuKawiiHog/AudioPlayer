7c715b1317ef34390bcc06dedb1f7029de56ad9a
eazyyz <eazyyyyyyz@gmail.com>
2025-09-29 12:12:05 +0500

    test

--- App.xaml.cs
+++ App.xaml.cs
@@ -8,5 +8,16 @@
 
             MainPage = new AppShell();
         }
+
+        protected override Window CreateWindow(IActivationState activationState)
+        {
+            var window = base.CreateWindow(activationState);
+
+            // минимальный размер
+            window.MinimumWidth = 1000;
+            window.MinimumHeight = 600;
+
+            return window;
+        }
     }
 }
