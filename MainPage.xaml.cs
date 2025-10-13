a6ac868420a8c3d29c173ffe5f2be9a3186fa49a
eazyyz <eazyyyyyyz@gmail.com>
2025-10-13 11:33:23 +0500

    add 3 page

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -1,12 +1,19 @@
-﻿namespace AudioPlayer
-{
-    public partial class MainPage : ContentPage
-    {
+﻿using Microsoft.Maui.Controls;
 
-        public MainPage()
-        {
-            InitializeComponent();
-        }
+namespace AudioPlayer;
+
+public partial class MainPage : ContentPage
+{
+    public MainPage()
+    {
+        InitializeComponent();
     }
 
+    private async void OnOpenFolderClicked(object sender, EventArgs e)
+    {
+        // тут потом открыть папку типо
+        //await Navigation.PushAsync(new PlayerPage(), animated: false);
+        Application.Current.MainPage = new PlayerPage();
+
+    }
 }
