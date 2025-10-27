4d697f7624726255afab80e7d1c6b61efe9c1531
SosikuKawiiHog <razdobarov_se@edu.surgu.ru>
2025-10-27 12:08:39 +0500

    Folder picker basic implementation

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -1,19 +1,39 @@
 ﻿using Microsoft.Maui.Controls;
+using CommunityToolkit.Maui.Storage;
 
 namespace AudioPlayer;
 
 public partial class MainPage : ContentPage
 {
+    private readonly IFolderPicker _folderPicker;
     public MainPage()
     {
         InitializeComponent();
+        _folderPicker = FolderPicker.Default;
     }
 
     private async void OnOpenFolderClicked(object sender, EventArgs e)
     {
-        // тут потом открыть папку типо
-        await Navigation.PushAsync(new PlayerPage(), animated: false);
-        //Application.Current.MainPage = new PlayerPage();
+        try
+        {
+            var result = await _folderPicker.PickAsync(CancellationToken.None);
 
+            if (result.IsSuccessful && result.Folder != null)
+            {
+                var folder = result.Folder;
+                folderPath.Text = $"Name: {folder.Name}\nPath: {folder.Path}";
+
+                // Переход на страницу плеера после выбора папки
+                await Navigation.PushAsync(new PlayerPage(), animated: false);
+            }
+            else
+            {
+                folderPath.Text = "Выбор папки отменён.";
+            }
+        }
+        catch (Exception ex)
+        {
+            folderPath.Text = $"Ошибка: {ex.Message}";
+        }
     }
 }
